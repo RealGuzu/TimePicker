bb2af902b89756ae5c36800d20205d80e873f1e2
RealGuzu <147645744+RealGuzu@users.noreply.github.com>
2024-03-07 13:27:41 -0500

    first commit

---
+++ app/src/main/java/com/example/timpicker/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.timpicker;
+
+import android.os.Bundle;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.sql.Time;
+
+public class MainActivity extends AppCompatActivity {
+
+    TimePicker timePicker;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        timePicker = findViewById(R.id.timePicker);
+        timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+                //Handle time changes
+
+                Toast.makeText(MainActivity.this, "Hour: "+ hourOfDay +"Minute: " +minute, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+}
